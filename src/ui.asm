beb61915224a496435c4fa59d16f86758d816ac7
TomasVolker <tomasvolker@gmail.com>
2015-11-15 14:05:55 -0200

    Subrutinas para escribir en pantalla

--- src/ui.asm
+++ src/ui.asm
@@ -1,32 +1,13 @@
 
+.CSEG
+
+UI_TEXT_GEPETTO:	.DB "Gepetto         ", 0
+UI_TEXT_1:			.DB "Primera linea   ", 0
+UI_TEXT_2:			.DB "Segunda linea   ", 0
 
 .DEF TEMP = R16
 
-.CSEG
-
-UI_TEXT: .DB "Gepetto", 0
-
-
 UI_INIT:
-	
-	;Delay 20ms
-	LDI R16, 150
-	LDI R17, 160
-	LDI R18, 2
-	CALL DELAY_3_REG
-	
-	CALL LCD_INIT
-	
-	RCALL UI_WAIT_LCD_READY
-	
-	CBI LCD_PORT, LCD_RS ;Enviar a registro de intrucciones
-	
-	LDI TEMP, 0x0F ;Display on, cursor on, blink on
-	CALL LCD_SEND_BYTE
-	
-	RET
-
-UI_FULL_INIT:
 	
 	CALL LCD_INIT
 	
@@ -36,10 +17,10 @@ UI_FULL_INIT:
 	LDI R18, 2
 	CALL DELAY_3_REG
 	
-	CBI LCD_PORT, LCD_RS ;Enviar a registro de intrucciones
+	CBI LCD_PORT, LCD_RS		;Enviar a registro de intrucciones
 
 	LDI TEMP, 0x30
-	CALL LCD_SEND_NIBBLE ;Interfaz 8bit
+	CALL LCD_SEND_NIBBLE		;Interfaz 8bit
 	
 	;Delay 4.1ms
 	LDI R16, 51
@@ -47,7 +28,7 @@ UI_FULL_INIT:
 	CALL DELAY_2_REG
 	
 	LDI TEMP, 0x30
-	CALL LCD_SEND_NIBBLE ;Interfaz 8bit
+	CALL LCD_SEND_NIBBLE		;Interfaz 8bit
 	
 	;Delay 100us
 	LDI R16, 21
@@ -55,40 +36,26 @@ UI_FULL_INIT:
 	CALL DELAY_2_REG
 	
 	LDI TEMP, 0x30
-	CALL LCD_SEND_NIBBLE ;Interfaz 8bit
-	
-	;Verificar delay entre instrucciones
+	CALL LCD_SEND_NIBBLE		;Interfaz 8bit
 	
 	RCALL UI_WAIT_LCD_READY
 	
 	LDI TEMP, 0x20
-	CALL LCD_SEND_NIBBLE ;Interfaz 4bit
-	
-	RCALL UI_WAIT_LCD_READY
-	
-	LDI TEMP, 0x28 ;Interfaz 4bit, 2 lineas, font 5x8
-	CALL LCD_SEND_BYTE
-	
-	RCALL UI_WAIT_LCD_READY
-	
-	LDI TEMP, 0x0C ;Display on, cursor off, blink off
-	CALL LCD_SEND_BYTE
-	
-	RCALL UI_WAIT_LCD_READY
-	
-	LDI TEMP, 0x01 ;Display clear
-	CALL LCD_SEND_BYTE
-	
-	RCALL UI_WAIT_LCD_READY
-	
-	LDI TEMP, 0x06 ;Increment, No display shift
-	CALL LCD_SEND_BYTE
+	CALL LCD_SEND_NIBBLE		;Interfaz 4bit
 	
 
-	LDI ZL, LOW(UI_TEXT<<1)
-	LDI ZH, HIGH(UI_TEXT<<1)
-	CALL UI_WRITE_P_STRING
-
+	LDI TEMP, 0x28				;Interfaz 4bit, 2 lineas, font 5x8
+	CALL UI_SEND_INSTRUCTION
+	
+	LDI TEMP, 0x0C				;Display on, cursor off, blink off
+	CALL UI_SEND_INSTRUCTION
+	
+	LDI TEMP, 0x01				;Display clear
+	CALL UI_SEND_INSTRUCTION
+	
+	LDI TEMP, 0x06				;Increment, No display shift
+	CALL UI_SEND_INSTRUCTION
+	
 	RET
 
 
@@ -98,10 +65,10 @@ UI_FULL_INIT:
 	
 UI_WAIT_LCD_READY:
 	
-	CBI LCD_PORT, LCD_RS ;Leer de registro de intrucciones
+	CBI LCD_PORT, LCD_RS		;Leer de registro de intrucciones
 	
 	CALL LCD_READ_BYTE
-	ANDI READ_REG, (1<<LCD_BF) ;Verificar el Busy Flag
+	ANDI READ_REG, (1<<LCD_BF)	;Verificar el Busy Flag
 	BRNE UI_WAIT_LCD_READY
 
 	RET
@@ -111,13 +78,29 @@ UI_WAIT_LCD_READY:
 
 .DEF CHAR_REG = R16
 	
+UI_SEND_INSTRUCTION:
+	
+	PUSH CHAR_REG
+	RCALL UI_WAIT_LCD_READY		;Esperar a que este listo
+	POP CHAR_REG
+	
+	CBI LCD_PORT, LCD_RS		;Escribir en registro de instrucciones
+	
+	CALL LCD_SEND_BYTE
+	
+	RET
+
+;Subrutina para enviar un caracter al LCD	
+
+.DEF CHAR_REG = R16
+	
 UI_WRITE_CHAR:
 	
 	PUSH CHAR_REG
-	RCALL UI_WAIT_LCD_READY ;Esperar a que este listo
+	RCALL UI_WAIT_LCD_READY		;Esperar a que este listo
 	POP CHAR_REG
 	
-	SBI LCD_PORT, LCD_RS ;Escribir en registro de datos
+	SBI LCD_PORT, LCD_RS		;Escribir en registro de datos
 	
 	CALL LCD_SEND_BYTE
 	
@@ -138,11 +121,37 @@ UI_WRITE_P_STRING:
 
 		RCALL UI_WRITE_CHAR
 		
-		RJMP UI_WRITE_P_STRING
+		RJMP UI_WRITE_P_STRING_LOOP
 
 	UI_WRITE_P_STRING_LOOP_BREAK:
 
 	POP ZH
 	POP ZL
 	RET
+
+
+;Subrutina para escribir un String a la primera linea LCD. El string es apuntado por Z y termina en 0
+
+.DEF TEMP = R16
+
+UI_WRITE_FIRST_LINE_P_STRING:
 	
+	LDI TEMP, (1<<7) | 0x00			;Cursor en cero
+	CALL UI_SEND_INSTRUCTION
+
+	CALL UI_WRITE_P_STRING
+
+	RET
+
+;Subrutina para escribir un String a la primera linea LCD. El string es apuntado por Z y termina en 0
+
+.DEF TEMP = R16
+
+UI_WRITE_SECOND_LINE_P_STRING:
+	
+	LDI TEMP, (1<<7) | 0x40			;Cursor en 0x40
+	CALL UI_SEND_INSTRUCTION
+
+	CALL UI_WRITE_P_STRING
+
+	RET
